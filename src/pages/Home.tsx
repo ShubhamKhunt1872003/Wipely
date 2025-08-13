24acd25e7a0619f5c1f5d91be8e191549812a886
Shubham Khunt <81526973+ShubhamKhunt1872003@users.noreply.github.com>
2025-08-13 08:59:34 +0200

    Change banner to Service.jpg

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -142,7 +142,7 @@ const toggleFAQ = (index:any) => {
       <section className="relative h-screen overflow-hidden">
         <div className="absolute inset-0">
           <img
-            src={kitchenImage}
+            src="/src/images/Service.jpg"
             alt="Professional Kitchen Cleaning"
             className="w-full h-full object-cover"
           />
