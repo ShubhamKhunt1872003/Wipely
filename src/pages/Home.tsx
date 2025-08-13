43b18670366df8623a3ee9b6ccc27dbd5996530e
Shubham Khunt <81526973+ShubhamKhunt1872003@users.noreply.github.com>
2025-08-13 08:44:42 +0200

    Change banner image to office.jpg

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,7 +20,7 @@ import {
   ChevronRight
 } from 'lucide-react';
 import postalCodes from '../data/postalcode.json';
-import bannerImage from '../images/kitchen.jpg';
+import bannerImage from '../images/office.jpg';
 
 const Home: React.FC = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
