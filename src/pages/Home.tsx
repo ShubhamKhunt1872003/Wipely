a0a3f66499eed9116a03f2cd8cf1ec345965639b
Shubham Khunt <shubhamkhunt029@gmail.com>
2025-11-15 21:52:42 +1100

    link tag add in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -573,12 +573,12 @@ const toggleFAQ = (index:any) => {
     <p className="text-lg mb-6 max-w-2xl mx-auto">
       Get sparkling results with Wipely ,  trusted by hundreds of happy customers across Melbourne. Fast, eco-friendly, and professional cleaning at your fingertips.
     </p>
-    <a
-      href="/book" // or use your booking route
-      className="inline-block bg-white text-emerald-600 font-semibold py-3 px-6 rounded-full shadow-md hover:bg-gray-100 transition duration-300"
-    >
-      Book Now
-    </a>
+   <Link
+  to="/book"   // MUST match your route path
+  className="inline-block bg-white text-emerald-600 font-semibold py-3 px-6 rounded-full shadow-md hover:bg-gray-100 transition duration-300"
+>
+  Book Now
+</Link>
   </div>
 </section>
     </div>
